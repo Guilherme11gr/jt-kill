596e68103ff7e89908ad773cc2c7030ecb8ffa3c
Guilherme Revoredo <37821225+Guilherme11gr@users.noreply.github.com>
2026-04-01 13:58:48 -0300

    feat(sdk): context usage bar + compact button (FLXO-344)

---
+++ packages/agent-sdk/dist/tokens-GGVVHJS3.js
@@ -0,0 +1,15 @@
+import {
+  createSummaryMessage,
+  estimateMessagesTokens,
+  estimateTokens,
+  summarizeHistory,
+  truncateHistory
+} from "./chunk-RJETFAKV.js";
+export {
+  createSummaryMessage,
+  estimateMessagesTokens,
+  estimateTokens,
+  summarizeHistory,
+  truncateHistory
+};
+//# sourceMappingURL=tokens-GGVVHJS3.js.map

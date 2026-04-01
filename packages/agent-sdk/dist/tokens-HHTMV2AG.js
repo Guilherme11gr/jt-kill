224ff863ec6ef852842f96442245c77b7d9c122d
Guilherme Revoredo <37821225+Guilherme11gr@users.noreply.github.com>
2026-04-01 13:58:52 -0300

    feat(sdk): context usage bar + compact button (FLXO-344)

---
+++ packages/agent-sdk/dist/tokens-HHTMV2AG.js
@@ -0,0 +1,15 @@
+import {
+  createSummaryMessage,
+  estimateMessagesTokens,
+  estimateTokens,
+  summarizeHistory,
+  truncateHistory
+} from "./chunk-GKUNIXJR.js";
+export {
+  createSummaryMessage,
+  estimateMessagesTokens,
+  estimateTokens,
+  summarizeHistory,
+  truncateHistory
+};
+//# sourceMappingURL=tokens-HHTMV2AG.js.map
